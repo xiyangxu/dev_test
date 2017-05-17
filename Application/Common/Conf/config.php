fec05aaf5a00c719cfd6d87bcd3d9e0c48c59f96
xiyangxu <389223520@qq.com>
2017-05-17 11:04:43 +0800

    bind module

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,4 +1,16 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+    'REDIS_HOST'   => '127.0.0.1',
+    'REDIS_PASSWD' => '',
+    'REDIS_PORT'   => '6379',
+    
+    'DB_TYPE'   => 'mysql', // 数据库类型
+    'DB_HOST'   => 'localhost', // 服务器地址
+    'DB_USER'   => 'root', // 用户名
+    'DB_PWD'    => '', // 密码
+    'DB_PORT'   => 3306, // 端口
+    'DB_CHARSET'=> 'utf8', // 字符集
+    'DB_NAME'   => 'gxgddbcenter', // 数据库名
+    'DB_PREFIX' => 'gxgd_tbl_', // 数据库表前缀
 );
